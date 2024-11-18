f1a3412bb49fe58764bbc8148199eaad876430c1
NhanB2105586 <nhanb2105586@student.ctu.edu.vn>
2024-11-18 13:20:15 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-
+const ApiError = require("./app/api-error");
 const app = express();
+const bookRouter = require("./app/routes/book.route");
+
 // Cho phép các yêu cầu từ mọi nguồn
 app.use(cors());
 
@@ -13,5 +15,22 @@ app.get("/", (req, res) => {
   // Trả về một response với dữ liệu JSON
   res.json({ message: "Welcome to contact book application." });
 });
+app.use("/api/book", bookRouter);
 
+// Xử lý lỗi 404 (Not Found)
+app.use((req, res, next) => {
+  // Code ở đây sẽ chạy khi không có route được định nghĩa nào
+  // khớp với yêu cầu. Gọi next(error) để chuyển sang middleware xử lý lỗi
+  return next(new ApiError(404, "Resource not found"));
+});
+
+// Định nghĩa middleware xử lý lỗi toàn cục
+app.use((err, req, res, next) => {
+  // Trong các đoạn code xử lý ở các route, gọi next(error) sẽ chuyển về middleware xử lý lỗi này
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
+// Export ứng dụng để sử dụng ở các file khác
 module.exports = app;
---
+++ app/routes/book.route.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const book = require("../controllers/book.controller");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(book.findAll)
+  .post(book.create)
+  .delete(book.deleteAll);
+
+router.route("/favorite").get(book.findAllFavorite);
+
+router
+  .route("/:id")
+  .get(book.findOne)
+  .put(book.update)
+  .delete(book.delete);
+
+module.exports = router;
